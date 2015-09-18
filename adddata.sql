b5f3ef6cdd1e770eeb0f44f4bec89430aac99bf5
Aoife Gildernew <A.Gildernew@kainos.com>
2015-09-18 12:35:06 +0100

    using auto increment

--- adddata.sql
+++ adddata.sql
@@ -1,20 +1,20 @@
-insert into owner values('Harold', 1);
-insert into owner values('Gwen', 2);
-insert into owner values('Benny', 3);
-insert into owner values('Diane', 4);
+insert into owner(ownername) values('Harold');
+insert into owner(ownername) values('Gwen');
+insert into owner(ownername) values('Benny');
+insert into owner(ownername) values('Diane');
 
-insert into species values('cat', 1);
-insert into species values('dog', 2);
-insert into species values('bird', 3);
-insert into species values('snake', 4);
-insert into species values('hamster', 5);
+insert into species(speciesname) values('cat');
+insert into species(speciesname) values('dog');
+insert into species(speciesname) values('bird');
+insert into species(speciesname) values('snake');
+insert into species(speciesname) values('hamster');
 
-insert into pet values('Fluffy', 1, 1, 1, 'f', '1993-02-04', null);
-insert into pet values('Claws', 2, 1, 2, 'm', '1994-03-17', null);
-insert into pet values('Buffy', 3, 2, 1,  'f', '1989-05-13', null);
-insert into pet values('Fang', 4, 2, 3,  'm', '1990-08-27', null);
-insert into pet values('Bowser', 5, 2, 4, 'm', '1979-08-31', '1995-07-29');
-insert into pet values('Chirpy', 6, 4, 2, null, '1997-12-09', null);
-insert into pet values('Whistler', 7, 3, 2,  null, '1997-12-09', null);
-insert into pet values('Slim', 8,4, 3,  'm', '1996-04-29', null);
-insert into pet values('Puffball', 9, 5, 4, 'f', '1999-03-30', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Fluffy',1, 1, 'f', '1993-02-04', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Claws', 1, 2, 'm', '1994-03-17', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Buffy', 2, 1,  'f', '1989-05-13', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Fang', 2, 3,  'm', '1990-08-27', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Bowser', 2, 4, 'm', '1979-08-31', '1995-07-29');
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Chirpy', 4, 2, null, '1997-12-09', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Whistler', 3, 2,  null, '1997-12-09', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Slim', 4, 3,  'm', '1996-04-29', null);
+insert into pet(petname, speciesID, ownerID, sex, birth, death) values('Puffball', 5, 4, 'f', '1999-03-30', null);
--- createstructure.sql
+++ createstructure.sql
@@ -1,5 +1,5 @@
-CREATE TABLE owner (ownername VARCHAR(20), ownerID INT(4));
-CREATE TABLE species (speciesname VARCHAR(20), speciesID INT(4));
-CREATE TABLE pet (petname VARCHAR(20), petID INT(4), speciesID INT(4), ownerID INT(4), sex CHAR(1), birth DATE, death DATE);
+CREATE TABLE owner (ownername VARCHAR(20), ownerID INT PRIMARY KEY AUTO_INCREMENT);
+CREATE TABLE species (speciesname VARCHAR(20), speciesID INT PRIMARY KEY AUTO_INCREMENT);
+CREATE TABLE pet (petname VARCHAR(20), petID INT PRIMARY KEY AUTO_INCREMENT, speciesID INT(4), ownerID INT(4), sex CHAR(1), birth DATE, death DATE);
 
 
