0ee80ed1977a754482a1a37f429e8d7921cca383
“Eoghan <eoghan.bradley@kainos.com>
2015-09-18 14:57:44 +0100

    made 2 constraints

--- createstructure.sql
+++ createstructure.sql
@@ -2,4 +2,13 @@ CREATE TABLE owner (ownername VARCHAR(20), ownerID INT PRIMARY KEY AUTO_INCREMEN
 CREATE TABLE species (speciesname VARCHAR(20), speciesID INT PRIMARY KEY AUTO_INCREMENT);
 CREATE TABLE pet (petname VARCHAR(20), petID INT PRIMARY KEY AUTO_INCREMENT, speciesID INT(4), ownerID INT(4), sex CHAR(1), birth DATE, death DATE);
 
+alter table pet  
+add constraint species_check
+foregin key (speciesID)
+references species (speciesID);
+ 
+alter table pet 
+add constraint owner_check 
+foreign key (ownerID)
+references owner (owner ID) ;
 
