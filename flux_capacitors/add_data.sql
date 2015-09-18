8e4261e6fe0b68a761e89f5311d6b139eb788f1b
Kainos User <c.duffy@kainos>
2015-09-18 12:42:57 +0100

    Potentially working data

--- flux_capacitors/add_data.sql
+++ flux_capacitors/add_data.sql
@@ -1,18 +1,23 @@
-insert into pet values('Fluffy', 'cat', 'f', '1993-02-04', null);
-insert into pet values('Claws', 'cat',  'm', '1994-03-17', null);
-insert into pet values('Buffy', 'dog', 'f', '1989-05-13', null);
-insert into pet values('Fang', 'dog', 'm', '1990-08-27', null);
-insert into pet values('Bowser','dog', 'm', '1979-08-31', '1995-07-29');
-insert into pet values('Chirpy', 'bird', null, '1997-12-09', null);
-insert into pet values('Whistler', 'bird', null, '1997-12-09', null);
-insert into pet values('Slim', 'snake', 'm', '1996-04-29', null);
-insert into pet values('Puffball', 'hamster', 'f', '1999-03-30', null);
+insert into pet (name, sex, birth, death) values ('Fluffy', 'f', '1993-02-04', null);
+insert into pet (name, sex, birth, death) values ('Claws',  'm', '1994-03-17', null);
+insert into pet (name, sex, birth, death) values ('Buffy', 'f', '1989-05-13', null);
+insert into pet (name, sex, birth, death) values ('Fang', 'm', '1990-08-27', null);
+insert into pet (name, sex, birth, death) values ('Bowser', 'm', '1979-08-31', '1995-07-29');
+insert into pet (name, sex, birth, death) values ('Chirpy', null, '1997-12-09', null);
+insert into pet (name, sex, birth, death) values ('Whistler', null, '1997-12-09', null);
+insert into pet (name, sex, birth, death) values ('Slim', 'm', '1996-04-29', null);
+insert into pet (name, sex, birth, death) values ('Puffball', 'f', '1999-03-30', null);
 
-insert into owner values('Harold');
-insert into owner values('Gwen');
-insert into owner values('Benny');
-insert into owner values('Diane');
+insert into owner (ownername) values ('Harold');
+insert into owner (ownername) values ('Gwen');
+insert into owner (ownername) values ('Benny');
+insert into owner (ownername) values ('Diane');
 
+insert into species (species) values ('cat');
+insert into species (species) values ('dog');
+insert into species (species) values ('bird');
+insert into species (species) values ('snake');
+insert into species (species) values ('hamster');
 
 
 
