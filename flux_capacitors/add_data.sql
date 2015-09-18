32fa377ed8a102a2a36493b10bf9c7c37d9f676e
Kainos User <c.duffy@kainos>
2015-09-18 11:33:30 +0100

    Removing unnecessary species table

--- flux_capacitors/add_data.sql
+++ flux_capacitors/add_data.sql
@@ -1,22 +1,18 @@
-insert into pet values('Fluffy', 'f', '1993-02-04', null);
-insert into pet values('Claws', 'm', '1994-03-17', null);
-insert into pet values('Buffy', 'f', '1989-05-13', null);
-insert into pet values('Fang', 'm', '1990-08-27', null);
-insert into pet values('Bowser', 'm', '1979-08-31', '1995-07-29');
-insert into pet values('Chirpy',  null, '1997-12-09', null);
-insert into pet values('Whistler', null, '1997-12-09', null);
-insert into pet values('Slim', 'm', '1996-04-29', null);
-insert into pet values('Puffball', 'f', '1999-03-30', null);
+insert into pet values('Fluffy', 'cat', 'f', '1993-02-04', null);
+insert into pet values('Claws', 'cat',  'm', '1994-03-17', null);
+insert into pet values('Buffy', 'dog', 'f', '1989-05-13', null);
+insert into pet values('Fang', 'dog', 'm', '1990-08-27', null);
+insert into pet values('Bowser','dog', 'm', '1979-08-31', '1995-07-29');
+insert into pet values('Chirpy', 'bird', null, '1997-12-09', null);
+insert into pet values('Whistler', 'bird', null, '1997-12-09', null);
+insert into pet values('Slim', 'snake', 'm', '1996-04-29', null);
+insert into pet values('Puffball', 'hamster', 'f', '1999-03-30', null);
 
 insert into owner values('Harold');
 insert into owner values('Gwen');
 insert into owner values('Benny');
 insert into owner values('Diane');
 
-insert into species values('cat');
-insert into species values('dog');
-insert into species values('bird');
-insert into species values('snake');
-insert into species hamster('hamster');
+
 
 
