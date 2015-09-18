7e15354693c3f5ca722e21a0c7f45dc176d54363
Joshua.Knight <j.knight@kainos.com>
2015-09-18 14:58:42 +0100

    Added constraints

--- createstructure.sql
+++ createstructure.sql
@@ -2,9 +2,9 @@ drop database if exists PetDB;
 create database PetDB;
 use PetDB;
 
-create table Pet (PetID varchar(20), OwnerID varchar(20), SpeciesID varchar(20), Petname varchar(20), sex char(1), birth DATE, death DATE);
-create table Owner (OwnerID varchar(20), OwnerName varchar(20)); 
-create table Species (SpeciesID varchar(20), SpeciesName varchar(20)); 
+create table Pet (PetID int primary key, OwnerID int, SpeciesID int, Petname varchar(20), sex char(1), birth DATE, death DATE);
+create table Owner (OwnerID int primary key, OwnerName varchar(20)); 
+create table Species (SpeciesID int primary key, SpeciesName varchar(20)); 
 
 
 
