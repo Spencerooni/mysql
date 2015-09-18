e65ce9ea8da0d580c0914ec1ffba9b184c4593cf
Liam Ferris <L.Ferris@kainos.com>
2015-09-18 12:50:07 +0100

    Deleted

--- createstructure.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE petname (pet_ID int(5) primary key, petname varchar(20), owner_ID int(5), species_ID int(5), sex char(1), birth date, death date);
-
-CREATE TABLE OWNER (owner_ID int(5) primary key, ownername varchar(20));
-
-CREATE TABLE SPECIES (species_ID int(5) primary key, speciesname varchar(20));
