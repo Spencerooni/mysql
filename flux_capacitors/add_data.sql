b92e004e9cf9a582e0819f9588442b6c5e3c913d
Kainos User <c.duffy@kainos>
2015-09-18 16:09:26 +0100

    Manually entered ownid and specid in pet table

--- flux_capacitors/add_data.sql
+++ flux_capacitors/add_data.sql
@@ -1,12 +1,12 @@
-insert into pet (name, sex, birth, death, ownid, specid) values ('Fluffy', 'f', '1993-02-04', null);
-insert into pet (name, sex, birth, death) values ('Claws',  'm', '1994-03-17', null);
-insert into pet (name, sex, birth, death) values ('Buffy', 'f', '1989-05-13', null);
-insert into pet (name, sex, birth, death) values ('Fang', 'm', '1990-08-27', null);
-insert into pet (name, sex, birth, death) values ('Bowser', 'm', '1979-08-31', '1995-07-29');
-insert into pet (name, sex, birth, death) values ('Chirpy', null, '1997-12-09', null);
-insert into pet (name, sex, birth, death) values ('Whistler', null, '1997-12-09', null);
-insert into pet (name, sex, birth, death) values ('Slim', 'm', '1996-04-29', null);
-insert into pet (name, sex, birth, death) values ('Puffball', 'f', '1999-03-30', null);
+insert into pet (name, sex, birth, death, ownid, specid) values ('Fluffy', 'f', '1993-02-04', null, 1, 1);
+insert into pet (name, sex, birth, death) values ('Claws',  'm', '1994-03-17', null, 1, 2);
+insert into pet (name, sex, birth, death) values ('Buffy', 'f', '1989-05-13', null, 2, 1);
+insert into pet (name, sex, birth, death) values ('Fang', 'm', '1990-08-27', null, 2, 3);
+insert into pet (name, sex, birth, death) values ('Bowser', 'm', '1979-08-31', '1995-07-29', 2, 4);
+insert into pet (name, sex, birth, death) values ('Chirpy', null, '1997-12-09', null, 4, 2);
+insert into pet (name, sex, birth, death) values ('Whistler', null, '1997-12-09', null, 3, 2);
+insert into pet (name, sex, birth, death) values ('Slim', 'm', '1996-04-29', null, 4, 3);
+insert into pet (name, sex, birth, death) values ('Puffball', 'f', '1999-03-30', null, 5, 4);
 
 insert into owner (ownername) values ('Harold');
 insert into owner (ownername) values ('Gwen');
