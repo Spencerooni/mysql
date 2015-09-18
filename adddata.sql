fc15dc54d8f7ac0cdb753a0850fe70f0d74468c1
“Eoghan <eoghan.bradley@kainos.com>
2015-09-18 11:48:13 +0100

    fixed syntax error

--- adddata.sql
+++ adddata.sql
@@ -11,10 +11,10 @@ insert into species values('hamster', 5);
 
 insert into pet values('Fluffy', 1, 1, 1, 'f', '1993-02-04', null);
 insert into pet values('Claws', 2, 1, 2, 'm', '1994-03-17', null);
-insert into pet values('Buffy', 3, 2, 1  'f', '1989-05-13', null);
-insert into pet values('Fang', 4, 2, 3  'm', '1990-08-27', null);
-insert into pet values('Bowser', 5, 2, 4 'm', '1979-08-31', '1995-07-29');
-insert into pet values('Chirpy', 6, 4, 2 null, '1997-12-09', null);
+insert into pet values('Buffy', 3, 2, 1,  'f', '1989-05-13', null);
+insert into pet values('Fang', 4, 2, 3,  'm', '1990-08-27', null);
+insert into pet values('Bowser', 5, 2, 4, 'm', '1979-08-31', '1995-07-29');
+insert into pet values('Chirpy', 6, 4, 2, null, '1997-12-09', null);
 insert into pet values('Whistler', 7, 3, 2,  null, '1997-12-09', null);
 insert into pet values('Slim', 8,4, 3,  'm', '1996-04-29', null);
 insert into pet values('Puffball', 9, 5, 4, 'f', '1999-03-30', null);
