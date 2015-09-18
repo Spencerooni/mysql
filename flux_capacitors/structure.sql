ba0562b5ef76b5d62429121a07a3f2887233ab5c
Kainos User <c.duffy@kainos>
2015-09-18 12:51:45 +0100

    Finished (hopefully) version

--- flux_capacitors/structure.sql
+++ flux_capacitors/structure.sql
@@ -1,4 +1,5 @@
 CREATE Database menagerie;
+use menagerie;
 CREATE Table pet (name VARCHAR(20), species VARCHAR(20), sex CHAR(1), birth DATE, death DATE, ownID int, specID int, petID int NOT NULL AUTO_INCREMENT, PRIMARY KEY(petID));
 CREATE Table owner (ownername VARCHAR(20), ownID int NOT NULL AUTO_INCREMENT, PRIMARY KEY (ownID));
 CREATE Table species (species VARCHAR(20), specID int NOT NULL AUTO_INCREMENT, PRIMARY KEY (specID));
