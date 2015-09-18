f36557c69d9329cefd789b9a7b9290b4dd75a38e
Joshua.Knight <j.knight@kainos.com>
2015-09-18 12:03:50 +0100

    Updated structure and data

---
+++ createstructure.sql
@@ -0,0 +1,10 @@
+Create database PetTable;
+
+
+create table Pet (PetID varchar(20), OwnerID varchar(20), SpeciesID varchar(20), Petname varchar(20), sex char(1), birth DATE, death DATE);
+create table Owner (OwnerID varchar(20), OwnerName varchar(20); 
+create table Species (SpeciesID varchar(20), SpeciesName varchar(20); 
+
+
+
+
