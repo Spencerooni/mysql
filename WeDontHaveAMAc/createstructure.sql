66de8caa591702949d0fc2f62d0e7b622fad9d6b
Liam Ferris <L.Ferris@kainos.com>
2015-09-18 14:55:09 +0100

    consraints

--- WeDontHaveAMAc/createstructure.sql
+++ WeDontHaveAMAc/createstructure.sql
@@ -3,3 +3,8 @@ CREATE TABLE petname (pet_ID int(5) primary key, petname varchar(20), owner_ID i
 CREATE TABLE OWNER (owner_ID int(5) primary key, ownername varchar(20));
 
 CREATE TABLE SPECIES (species_ID int(5) primary key, speciesname varchar(20));
+
+ALTER TABLE petname ADD CONSTRAINT FK_owners FOREIGN KEY (species_ID) REFERENCES SPECIES (species_ID);
+
+ALTER TABLE petname ADD CONSTRAINT FK_owners FOREIGN KEY (owner_ID) REFERENCES OWNER (owner_ID);
+
