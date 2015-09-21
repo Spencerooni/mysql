3dc116e539a8b7643381308b9c40c92063a58a65
Eamon Compston <e.compston@kainos.com>
2015-09-21 10:17:33 +0100

    EAMON + EOGHAN4

--- david/addData.sql
+++ david/addData.sql
@@ -16,12 +16,12 @@ insert into species(id, name) values
 (5, 'hamster');
 
 insert into pet(name, sex, birth, death, owner_id, species_id, neutered, price) values
-('Fluffy', 'f', '1993-02-04', null, 1, 2, 'Y', 21.12), -- harold, cat
-('Claws', 'm', '1994-03-17', null, 2, 2,'Y', 32.23), -- gwen, cat
-('Buffy', 'f', '1989-05-13', null, 1, 1,'Y', 33.21), -- harold, dog
-('Fang', 'm', '1990-08-27', null, 3, 1,'Y', 99.90), -- benny, dog
-('Bowser', 'm',  '1979-08-31', '1995-07-29', 4, 1,'N', 32.23), -- diane, dog
-('Chirpy',  null, '1997-12-09', null, 2, 3,'N', 44.87), -- gwen, bird
-('Whistler',  null, '1997-12-09', null, 2, 3, 'N', 23.78), -- gwen, bird
-('Slim', 'm', '1996-04-29', null, 3, 4, 'N', 23.78), -- benny, snake
-('Puffball', 'f', '1999-03-30', null, 4, 5, 'N', 23.77); -- diane, hamster
+('Fluffy', 'f', '1993-02-04', null, 1, 2, '1', 21.12), -- harold, cat
+('Claws', 'm', '1994-03-17', null, 2, 2,'1', 32.23), -- gwen, cat
+('Buffy', 'f', '1989-05-13', null, 1, 1,'1', 33.21), -- harold, dog
+('Fang', 'm', '1990-08-27', null, 3, 1,'1', 99.90), -- benny, dog
+('Bowser', 'm',  '1979-08-31', '1995-07-29', 4, 1,'0', 32.23), -- diane, dog
+('Chirpy',  null, '1997-12-09', null, 2, 3,'0', 44.87), -- gwen, bird
+('Whistler',  null, '1997-12-09', null, 2, 3, '1', 23.78), -- gwen, bird
+('Slim', 'm', '1996-04-29', null, 3, 4, '0', 23.78), -- benny, snake
+('Puffball', 'f', '1999-03-30', null, 4, 5, '0', 23.77); -- diane, hamster
