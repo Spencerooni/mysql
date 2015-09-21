d5ba96d5ff163e8832c334775354a80ee7fc2c84
Aoife Gildernew <A.Gildernew@kainos.com>
2015-09-21 12:23:02 +0100

    mysql exercise answers

--- david/addData.sql
+++ david/addData.sql
@@ -1,7 +1,7 @@
 
+delete from pet;
 delete from owner;
 delete from species;
-delete from pet;
 
 insert into owner(id, name) values
 (1, 'Harold'),
@@ -16,13 +16,13 @@ insert into species(id, name) values
 (4, 'snake'),
 (5, 'hamster');
 
-insert into pet(name, sex, birth, death, owner_id, species_id) values
-('Fluffy', 'f', '1993-02-04', null, 1, 2), -- harold, cat
-('Claws', 'm', '1994-03-17', null, 2, 2), -- gwen, cat
-('Buffy', 'f', '1989-05-13', null, 1, 1), -- harold, dog
-('Fang', 'm', '1990-08-27', null, 3, 1), -- benny, dog
-('Bowser', 'm',  '1979-08-31', '1995-07-29', 4, 1), -- diane, dog
-('Chirpy',  null, '1997-12-09', null, 2, 3), -- gwen, bird
-('Whistler',  null, '1997-12-09', null, 2, 3), -- gwen, bird
-('Slim', 'm', '1996-04-29', null, 3, 4), -- benny, snake
-('Puffball', 'f', '1999-03-30', null, 4, 5); -- diane, hamster
+insert into pet(name, sex, birth, death, owner_id, species_id, neutered, price) values
+('Fluffy', 'f', '1993-02-04', null, 1, 2, 1, 12.99), -- harold, cat
+('Claws', 'm', '1994-03-17', null, 2, 2, 0, 10.00), -- gwen, cat
+('Buffy', 'f', '1989-05-13', null, 1, 1, 0, 15), -- harold, dog
+('Fang', 'm', '1990-08-27', null, 3, 1, 1, 19.99), -- benny, dog
+('Bowser', 'm',  '1979-08-31', '1995-07-29', 4, 1, 0, 29.99), -- diane, dog
+('Chirpy',  null, '1997-12-09', null, 2, 3, 0, 30.50), -- gwen, bird
+('Whistler',  null, '1997-12-09', null, 2, 3, 1, 19.00), -- gwen, bird
+('Slim', 'm', '1996-04-29', null, 3, 4, 0, 50.00), -- benny, snake
+('Puffball', 'f', '1999-03-30', null, 4, 5, 0, 10.00); -- diane, hamster
