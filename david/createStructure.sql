88fb0ff2046f6555c0df3b7ba15a6fd0da6393bf
David Spence <d.spence@kainos.com>
2015-09-21 07:34:28 +0100

    Adding constraints to owner and species

--- david/createStructure.sql
+++ david/createStructure.sql
@@ -6,3 +6,6 @@ create table owner (id int primary key auto_increment, name varchar(20));
 
 create table species (id int primary key auto_increment, name varchar(20));
 
+alter table pet add constraint fk_owner_id foreign key(owner_id) references owner (id);
+alter table pet add constraint fk_species_id foreign key(species_id) references species (id);
+
