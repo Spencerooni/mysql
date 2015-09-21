a0d87dbf267e0602beab3664267b683d57fa8a78
Liam Ferris <L.Ferris@kainos.com>
2015-09-21 12:39:29 +0100

    fixed %s

--- answers.sql
+++ answers.sql
@@ -12,8 +12,8 @@ select distinct petname, count(*) from pet group by petname;
 SELECT distinct owner.ownername from owner left outer join pet on pet.ownerID = owner.ownerID where petname is null;
 select petname, price from pet where price > 9.99 AND price < 19.99 order by price desc;
 SELECT pet.petname, owner.ownername from owner join pet on pet.ownerID = owner.ownerID where ownername = 'John Smith';
-SELECT pet.petname, owner.ownername from owner join pet on pet.ownerID = owner.ownerID where ownername like '%John%';
-SELECT pet.petname, owner.ownername from owner join pet on pet.ownerID = owner.ownerID where ownername like '%Smith%' OR ownername like '%Smyth%';
+SELECT pet.petname, owner.ownername from owner join pet on pet.ownerID = owner.ownerID where ownername like 'John%';
+SELECT pet.petname, owner.ownername from owner join pet on pet.ownerID = owner.ownerID where ownername like '%Smith' OR ownername like '%Smyth';
 SELECT pet.petname, owner.ownername from owner join pet on pet.ownerID = owner.ownerID where ownername like '%av%';
 
 
