b5d8d4c1411ff26608355c1e1038379c519c4d02
Peter Bonar <p.bonar@kainos.com>
2015-09-21 11:27:30 +0100

    updated changes

---
+++ CreateStructure.sql
@@ -0,0 +1,8 @@
+create table pet (id int primary key auto_increment, name varchar(20), sex char(1), birth date, death date, owner_id int, species_id int, price decimal(8,2), neutered boolean);
+
+create table owner (id int primary key auto_increment, name varchar(20));
+
+create table species (id int primary key auto_increment, name varchar(20));
+
+alter table pet add constraint fk_owner_id foreign key(owner_id) references owner (id);
+alter table pet add constraint fk_species_id foreign key(species_id) references species (id);
