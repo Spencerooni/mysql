fec37e8d7b7a1b7b0e9861a50f4e51142e8e09f7
Kainos User <c.duffy@kainos>
2015-09-22 09:39:54 +0100

    Update now accounts for null values (pets without owners)

--- david/Update1.sql
+++ david/Update1.sql
@@ -1,5 +1,5 @@
 Create Table Multiple_Owners (pet_id int, own_id int, PRIMARY KEY (pet_id, own_id));
-insert into Multiple_Owners (pet_id, own_id) (select pet.id, pet.owner_id from pet);
+insert into Multiple_Owners (pet_id, own_id) (select pet.id, pet.owner_id from pet where pet.owner_id is not null);
 
 Alter table pet drop foreign key fk_owner_id;
 Alter table pet drop column owner_id;
