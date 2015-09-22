cfe3ce0a6afcf39fb83618e70d20c17f2982f222
Eoghan Bradley <eoghan.bradley@kainos.com>
2015-09-22 09:33:10 +0100

    fixed syntax error in update script

--- david/updatescript.sql
+++ david/updatescript.sql
@@ -5,16 +5,15 @@ create table ownpet(ownpet_id int primary key auto_increment, owner_id int, pet_
 
 INSERT INTO ownpet (owner_id, pet_id)
 SELECT pet.owner_id,
- pet_id from pet; 
+ id from pet; 
 
 ALTER TABLE `ownpet` ADD UNIQUE `unique_index`(`pet_id`, `owner_id`);
 
-ALTER TABLE ownpet
-   MODIFY owner_id int NOT NULL;
+	ALTER TABLE ownpet
+   	MODIFY owner_id int NOT NULL;
 
-ALTER TABLE ownpet
-   MODIFY pet_id int NOT NULL;
-
+	ALTER TABLE ownpet
+   	MODIFY pet_id int NOT NULL;
 
 alter table pet
 drop owner_id ; 
@@ -22,6 +21,5 @@ drop owner_id ;
 alter table ownpet add constraint fk_owner_id foreign key(owner_id) references owner(id);
 alter table ownpet add constraint fk_pet_id foreign key(pet_id) references pet(id); 
 
-select owner.owner_id, pet.pet_id from owner, pet left outer join
 
 
